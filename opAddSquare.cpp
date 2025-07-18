3b921c58b811c8fe53eb0106e7ac314bc79c8976
HaneenGamill <s-haneen.gamil@zewailcity.edu.eg>
2025-07-18 21:53:10 +0300

    opAddSquare.cpp

---
+++ opAddSquare.cpp
@@ -0,0 +1,62 @@
+#include "opAddSquare.h"
+
+#include "square.h"
+
+#include "controller.h"
+
+#include "GUI/GUI.h"
+
+opAddSquare::opAddSquare(controller* pCont) :operation(pCont)
+{
+}
+opAddSquare::~opAddSquare()
+{
+}
+
+//Execute the operation
+void opAddSquare::Execute()
+{
+	Point P1, P2;
+
+	//Get a Pointer to the Input / Output Interfaces
+	GUI* pUI = pControl->GetUI();
+
+	pUI->PrintMessage("New Square: Click at first corner");
+	//Read 1st corner and store in point P1
+	pUI->GetPointClicked(P1.x, P1.y);
+
+
+
+	string msg = "First corner is at (" + to_string(P1.x) + ", " + to_string(P1.y) + " )";
+	msg += " ... Click at second corner";
+	pUI->PrintMessage(msg);
+	//Read 2nd corner and store in point P2
+	pUI->GetPointClicked(P2.x, P2.y);
+	pUI->ClearStatusBar();
+
+	//Preapre all rectangle parameters
+	GfxInfo RectGfxInfo;
+
+	//get drawing, filling colors and pen width from the interface
+	RectGfxInfo.DrawClr = pUI->getCrntDrawColor();
+	RectGfxInfo.FillClr = pUI->getCrntFillColor();
+	RectGfxInfo.BorderWdth = pUI->getCrntPenWidth();
+
+
+	RectGfxInfo.isFilled = false;	//default is not filled
+	RectGfxInfo.isSelected = false;	//defualt is not selected
+
+
+	//Create a rectangle with the above parameters
+	Square* R = new Square(P1, P2, RectGfxInfo);
+
+
+
+
+	//Get a pointer to the graph
+	Graph* pGr = pControl->getGraph();
+
+	//Add the square to the list of shapes
+	pGr->Addshape(R);
+
+}
